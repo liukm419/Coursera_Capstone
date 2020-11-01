e1bdc9a1b14af190d6dfa664c62bdaebf41f0d5a
Ken Liu <kenliu419@gmail.com>
2020-10-31 21:03:55 -0700

    the required documents and work for the final capstone

---
+++ FinalCapstone/CapstoneProject.py
@@ -0,0 +1,7 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticket import NullFormatter
+import pandas as pd
+import matplotlib.ticker as ticket
+from sklearn import preprocessing
